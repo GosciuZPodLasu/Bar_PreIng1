59e3fec3c49665cd162c4659f09a6511f5b4f625
Yenni-tdr <83454001+Yenni-tdr@users.noreply.github.com>
2021-05-12 10:43:43 +0200

    Add files via upload

--- commandecocktail.h
+++ commandecocktail.h
@@ -3,34 +3,34 @@
 
 void print(BoissonNonAlcoolisee tabA[N],BoissonAlcoolisee tabB[N]){
     for (int i= 0;i<N;i++){
-        printf("%d.%s, ",i,tabA[i].nom);
+        printf("%d. %s\n",i,tabA[i].nom);
     }
     for (int i=0;i<N-1;i++){
-        printf("%d.%s, ",i+N,tabB[i].nom);
+        printf("%d. %s\n",i+N,tabB[i].nom);
     }
     printf("19.%s\n",tabB[N-1].nom);
 }
 
 int commande_cocktail(BoissonNonAlcoolisee tabA[N],BoissonAlcoolisee tabB[N], panier panier[30]){
     int nombre;
-    printf("Tu a choisi un Cocktail, tu peut faire une combinaison de maximum 3 boisson.\n");
-    printf("choisi combien de combinaison tu a envie entre (2-3) : ");
+    printf("\nTu as choisi un Cocktail, tu peux faire une combinaison de maximum 3 boissons\n");
+    printf("Combien de boissons ? [Entre 2 et 3] : \n");
     scanf("%d",&nombre);
     if(nombre<2 || nombre>3){
-        printf("Tu a pas choisi un bon nombre de boisson !\nEssai une deuxieme fois.\n");
+        printf("Tu n'as pas choisi le bon nombre de boissons !\nEssaie une deuxieme fois.\n");
         commande_cocktail(tabA, tabB, panier);
         return 0;
     }
-    printf("tu a choisi de faire une combinaison de %d boisson.\n tu a le chois entre cella :", nombre);
+    printf("Tu as choisi de faire une combinaison de %d boissons.\nTu as le choix parmis ces boissons : \n", nombre);
     print(tabA,tabB);
     int ChoixMix[nombre], verif=0,choix,erreur;
     while(verif!=nombre){
-        printf("Quelle est t'a %d boisson? : ",(verif+1));
+        printf("Quelle est ta boisson numero %d ? : ",(verif+1));
         scanf("%d",&choix);
         erreur=0;
         for(int i=0;i<=verif;i++){
             if (ChoixMix[i]==choix){
-                printf("TU ne peut pas choisire la meme boisson deux fois");
+                printf("Tu ne peux pas choisir la meme boisson deux fois\n");
                 erreur++;
             }
         }
@@ -39,7 +39,7 @@ int commande_cocktail(BoissonNonAlcoolisee tabA[N],BoissonAlcoolisee tabB[N], pa
             verif++;
         }      
     }
-    printf("tu a choisi les boisson numero= ");
+    printf("Tu as choisi les boissons : ");
     for(int i=0;i<nombre;i++){
         printf("%d ",ChoixMix[i]);
     }
